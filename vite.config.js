ac3d547f4bc9825fba7bb8cda4df398b6c94f35a
Siddhu123456 <mekalasidhu74@gmail.com>
2025-08-27 16:40:35 +0530

    corrected config.vite.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,   // exposes server to network / StackBlitz
-    port: 5173,   // default Vite port
-  },
 })
-
