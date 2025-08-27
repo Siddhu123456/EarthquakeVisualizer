86d26eb224be3d07a3f894c64010874f042f7a72
Siddhu123456 <mekalasidhu74@gmail.com>
2025-08-27 18:31:36 +0530

    updated the map with giving the separate file mapConfig

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 import ResetViewButton from '../ResetViewButton/ResetViewButton.jsx';
 import LoadingDialog from '../LoadingDialog/LoadingDialog.jsx';
+import mapConfigs from '../../mapConfigs.js';
 import './MapView.css';
 
 // Fix for default Leaflet markers in bundled environments
@@ -24,26 +25,7 @@ const MapView = () => {
     const filterState = useSelector((state) => state.earthquakeFilter);
     const { activeMaptype, timeframe } = filterState;
 
-    // Map type configurations
-    const mapConfigs = {
-        street: {
-            url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-        },
-        satellite: {
-            url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-            attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-        },
-        terrain: {
-            url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
-            attribution: "Map data: &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, <a href='http://viewfinderpanoramas.org'>SRTM</a> | Map style: &copy; <a href='https://opentopomap.org'>OpenTopoMap</a> (<a href='https://creativecommons.org/licenses/by-sa/3.0/'>CC-BY-SA</a>)"
-        },
-        dark: {
-            url: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-            attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/attributions'>CARTO</a>"
-        }
-    };
-
+    //fetching the data from api
     const fetchEarthquakes = async (endpoint) => {
         const startTime = Date.now();
         
